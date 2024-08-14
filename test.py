f8b84f1760b35d656d95fc2ba97a73a987f0d45b
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-08-14 01:26:24 -0500

    test

--- test.py
+++ test.py
@@ -99,7 +99,6 @@ class Player(Animation):
 
     def draw(self, screen):
         if self.current_animation:
-            print("hello")
             screen.blit(self.current_animation.get_current_frame(), (self.x, self.y))
 
     def move(self, dx, dy):

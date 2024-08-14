b568f87ad9171aca37a45341e948c9e90d33359b
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-08-14 01:26:42 -0500

    test

--- test.py
+++ test.py
@@ -172,7 +172,7 @@ def main():
         # Update the player's animation and draw the updated player
         player.update()
         WIN.blit(BG, (0, 0))  # Clear screen with background image
-        player.draw(WIN)      # Draw the player on the screen
+        player.draw(BG)      # Draw the player on the screen
         draw(elapsed_time, stars)  # Draw other game elements
         pygame.display.flip()  # Update the display
 

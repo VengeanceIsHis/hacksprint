07f23fa99159ffad987d9b87baaccd485ffc5c5b
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-08-14 00:03:17 -0500

    test

--- test.py
+++ test.py
@@ -54,6 +54,7 @@ class Player(pygame.sprite.Sprite):
     def set_animation(self, animation_type):
         if animation_type in self.animations and self.animations[animation_type]:
             self.current_animation = Animation(self.animations[animation_type], frame_rate=300)
+            print("HIIIIIII")
     
     def load_frames(self):
         for animation_type in self.animations:

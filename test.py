14bebda9a3cc8654e7b25ce6d819e21b1abe8ffa
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-08-14 12:38:44 -0500

    Test

--- test.py
+++ test.py
@@ -73,7 +73,7 @@ def main():
     pygame.init()
     run = True
 
-    player = Idle(800, 400)
+    player = Idle(900, 400)
     moving_sprites = pygame.sprite.Group()
     moving_sprites.add(player)
     clock = pygame.time.Clock()

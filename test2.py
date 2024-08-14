b3cd7f54d440a25b9868284fb7e2e26059cf6630
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-08-14 00:15:48 -0500

    fixed

--- test2.py
+++ test2.py
@@ -1,7 +1,85 @@
 import pygame as pg
 import random
 import time
+import os
 
+class Player(pg.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.width = 64
+        self.height = 64
+        self.x = 100
+        self.y = 100
+        self.rect = pg.Rect(self.x, self.y, self.width, self.height)
+
+        # Load frames for the animation
+        self.animations = {
+            'idle': [],
+            'run_left': [],
+            'run_right': []
+        }
+        self.load_frames()
+        self.current_animation = None
+
+    def set_animation(self, animation_type):
+        if animation_type in self.animations and self.animations[animation_type]:
+            self.current_animation = Animation(self.animations[animation_type], frame_rate=300)
+    
+    def load_frames(self):
+        for animation_type in self.animations:
+            animation = animation_type
+            if animation == 'run_left' or animation == 'run_right':
+                animation = 'run'
+            frames = []
+            folder_path = os.path.join('assets', 'animations', 'Knight', animation)
+            for filename in sorted(os.listdir(folder_path)):
+                if filename.endswith('.png'):
+                    frame_path = os.path.join(folder_path, filename)
+                    frame = pg.image.load(frame_path)
+                    frame = pg.transform.scale(frame, (self.width, self.height))
+                    frames.append(frame)
+            self.animations[animation_type] = frames
+
+    def update(self):
+        if self.current_animation:
+            self.current_animation.update()
+
+    def get_current_frame(self):
+        # Return the current frame from the animation
+        if self.current_animation:
+            return self.current_animation.get_current_frame()
+        return None
+
+    def draw(self, screen):
+        frame = self.get_current_frame()
+        if frame:
+            screen.blit(frame, (self.x, self.y))
+            print("HIIII")
+
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+        self.rect.topleft = (self.x, self.y)
+
+class Animation:
+    def __init__(self, frames, frame_rate, flipped=False):
+        self.frames = frames
+        self.frame_rate = frame_rate
+        self.current_frame = 0
+        self.last_update = pg.time.get_ticks()
+        self.flipped = flipped
+
+    def update(self):
+        now = pg.time.get_ticks()
+        if now - self.last_update > self.frame_rate:
+            self.current_frame = (self.current_frame + 1) % len(self.frames)
+            self.last_update = now
+
+    def get_current_frame(self):
+        frame = self.frames[self.current_frame]
+        if self.flipped:
+            return pg.transform.flip(frame, True, False)
+        return frame
 def main():
     pg.init()
     WIDTH, HEIGHT = 800, 600
